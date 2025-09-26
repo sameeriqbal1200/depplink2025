2d311bb323864a4ca6ac7c8f48402cace9ab663c
REHANTAHIR111 <rt3027501@gmail.com>
2025-09-26 19:46:06 +0500

    updatd

--- app/[lang]/components/LoyaltyHistoryList.tsx
+++ app/[lang]/components/LoyaltyHistoryList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useContext } from "react";
+import React from "react";
 
 export default function LoyaltyHistoryList(props: any) {
     const loyalData = props?.data;
