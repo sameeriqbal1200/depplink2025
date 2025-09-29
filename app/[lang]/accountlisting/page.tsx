0ff9af5881880cf83e1c296422263da5b8337d68
REHANTAHIR111 <rt3027501@gmail.com>
2025-09-29 15:31:47 +0500

    Update page.tsx

--- app/[lang]/accountlisting/page.tsx
+++ app/[lang]/accountlisting/page.tsx
@@ -207,7 +207,7 @@ export default function AccountListing() {
                             </h2>
 
                         </div>
-                        <svg height="26" viewBox="0 0 24 24" width="26" className="rotate-180" xmlns="http://www.w3.org/2000/svg" id="fi_2722991"><g id="_17" data-name="17"><path d="m15 19a1 1 0 0 1 -.71-.29l-6-6a1 1 0 0 1 0-1.41l6-6a1 1 0 0 1 1.41 1.41l-5.29 5.29 5.29 5.29a1 1 0 0 1 -.7 1.71z"></path></g></svg>
+                        <svg height="26" viewBox="0 0 24 24" width="26" className={lang === 'ar' ? "" : "rotate-180"} xmlns="http://www.w3.org/2000/svg" id="fi_2722991"><g id="_17" data-name="17"><path d="m15 19a1 1 0 0 1 -.71-.29l-6-6a1 1 0 0 1 0-1.41l6-6a1 1 0 0 1 1.41 1.41l-5.29 5.29 5.29 5.29a1 1 0 0 1 -.7 1.71z"></path></g></svg>
                     </Link>
                     {
                         localStorage.getItem("userid") ?
