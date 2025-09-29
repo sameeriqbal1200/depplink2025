a52b584c5f7911e3997c4967559a863a9a4b7ebe
Muhammad Usman Siddiqui <tamkeenstores@gmail.com>
2025-09-29 12:19:35 +0300

    update

--- app/[lang]/components/NewHomePageComp/CategoriesHomeMobile.tsx
+++ app/[lang]/components/NewHomePageComp/CategoriesHomeMobile.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { Scrollbar, Mousewheel, Grid, FreeMode } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css/free-mode';
-import 'swiper/css/scrollbar';
 import './scrollBar.css';
 import 'swiper/css/grid';
+import Link from 'next/link';
+import 'swiper/css/free-mode';
+import 'swiper/css/scrollbar';
+import Image from 'next/image';
 import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar, Mousewheel, Grid, FreeMode } from "swiper/modules";
 // import './styles.css';
 
 export default function CategoriesHomeMobile(props: any) {
@@ -35,66 +35,63 @@ export default function CategoriesHomeMobile(props: any) {
                 </Link>
             </div>
             {isClient && (
-                <>
-                    <Swiper
-                        direction="horizontal"
-                        slidesPerView={5}  // 2 slides per row
-                        spaceBetween={8}  // Space between slides
-                        scrollbar={{
-                            draggable: false,
-                            hide: false, // Show scrollbar
-                        }}
-                        mousewheel={{
-                            forceToAxis: true,
-                            releaseOnEdges: true,
-                            sensitivity: 2,
-                            eventsTarget: ".swiper-wrapper", // Mousewheel events will be attached to swiper wrapper
-                        }}
-                        grid={{
-                            rows: 2,
-                            fill: "row",
-                        }}
-                        freeMode={true}
-                        // speed={5000} // Smooth transition speed for slide changes
-                        modules={[Scrollbar, Mousewheel, Grid, FreeMode]}  // Use only Scrollbar, Mousewheel, and Grid
-                        className="mySwiper !pb-6"
-                    >
-                        {categories.length > 0 ? (
-                            categories.map((category: any, i: number) => (
-                                <SwiperSlide key={i} className="">
-                                    <div>
-                                        <div className=''>
-                                            <Link prefetch={false} scroll={false} href={`${origin}/${props?.lang}/category/${category?.slug}`} className="rounded-md">
-                                                <Image
-                                                    src={category?.image ? `${NewMedia}${category?.image?.image}` : ''}
-                                                    alt={`${renderCategoryName(category)}-${i + 11}`}
-                                                    title={renderCategoryName(category)}
-                                                    width={0}
-                                                    height={0}
-                                                    quality={100}
-                                                    sizes='(max-width: 140px) 100vw, (max-width: 1068px) 1150px, (max-width: 1024px) 1, 100vw'
-                                                    priority={true}
-                                                    className="mx-auto h-[4.8rem] w-full border rounded-xl shadow-sm border-[#ff7b345e]"
-                                                />
-                                                <h2 className="font-bold mt-2 text-[0.65rem] xl:text-xs line-clamp-1 md:line-clamp-2 leading-4 text-xs text-center">
-                                                    {renderCategoryName(category)}
-                                                </h2>
-                                            </Link>
-                                        </div>
+                <Swiper
+                    direction="horizontal"
+                    slidesPerView={5}  // 2 slides per row
+                    spaceBetween={8}  // Space between slides
+                    scrollbar={{
+                        draggable: false,
+                        hide: false, // Show scrollbar
+                    }}
+                    mousewheel={{
+                        forceToAxis: true,
+                        releaseOnEdges: true,
+                        sensitivity: 2,
+                        eventsTarget: ".swiper-wrapper", // Mousewheel events will be attached to swiper wrapper
+                    }}
+                    grid={{
+                        rows: 2,
+                        fill: "row",
+                    }}
+                    freeMode={true}
+                    modules={[Scrollbar, Mousewheel, Grid, FreeMode]}  // Use only Scrollbar, Mousewheel, and Grid
+                    className="mySwiper !pb-6"
+                >
+                    {categories.length > 0 ? (
+                        categories.map((category: any, i: number) => (
+                            <SwiperSlide key={i} className="">
+                                <div>
+                                    <div className=''>
+                                        <Link prefetch={false} scroll={false} href={`${origin}/${props?.lang}/category/${category?.slug}`} className="rounded-md">
+                                            <Image
+                                                src={category?.image ? `${NewMedia}${category?.image?.image}` : ''}
+                                                alt={`${renderCategoryName(category)}-${i + 11}`}
+                                                title={renderCategoryName(category)}
+                                                width={0}
+                                                height={0}
+                                                quality={100}
+                                                sizes='(max-width: 140px) 100vw, (max-width: 1068px) 1150px, (max-width: 1024px) 1, 100vw'
+                                                priority={true}
+                                                className="mx-auto h-auto bg-white w-full border rounded-xl shadow-sm border-[#ff7b345e]"
+                                            />
+                                            <h2 className="font-bold mt-2 text-[0.65rem] xl:text-xs line-clamp-1 md:line-clamp-2 leading-4 text-xs text-center">
+                                                {renderCategoryName(category)}
+                                            </h2>
+                                        </Link>
                                     </div>
-                                </SwiperSlide>
-                            ))
-                        ) : (
-                            Array.from({ length: 16 }).map((_, index) => (
-                                <SwiperSlide className="p-0.5" key={index}>
-                                    <div className="bg-white rounded-lg shadow-md text-center text-primary animate-pulse w-full">
-                                        <div className="rounded-md bg-dark/10 p-2.5 h-28 lg:h-[13.5rem] w-full"></div>
-                                    </div>
-                                </SwiperSlide>
-                            ))
-                        )}
-                    </Swiper>
-                </>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    ) : (
+                        Array.from({ length: 16 }).map((_, index) => (
+                            <SwiperSlide className="p-0.5" key={index}>
+                                <div className="bg-white rounded-lg shadow-md text-center text-primary animate-pulse w-full">
+                                    <div className="rounded-md bg-dark/10 p-2.5 h-28 lg:h-[13.5rem] w-full"></div>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    )}
+                </Swiper>
             )}
         </div>
     );
