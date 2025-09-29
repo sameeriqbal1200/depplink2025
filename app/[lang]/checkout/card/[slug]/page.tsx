4b5ade81996aa0b1722a0e4635d7891729a88fb9
Irfan Web <hi0487535@gmail.com>
2025-09-29 16:41:51 +0500

    card page fix

--- app/[lang]/checkout/card/[slug]/page.tsx
+++ app/[lang]/checkout/card/[slug]/page.tsx
@@ -1,26 +1,20 @@
 "use client"; // This is a client component 👈🏽
 
+import React from 'react';
 import dynamic from 'next/dynamic'
-import { Api } from '../../../api/Api'
-import React, { useEffect, useState } from 'react'
-import { getDictionary } from "../../../dictionaries"
+import { useApp } from '@/app/_ctx/AppContext';
 
-const MobileHeader = dynamic(() => import('../../../components/MobileHeader'), { ssr: true })
+const MobileHeader = dynamic(() => import('@/components/MobileHeader'), { ssr: true })
 
-export default function Card({ params }: { params: { lang: string, slug: any } }) {
-    const [dict, setDict] = useState<any>([]);
-    useEffect(() => {
-        (async () => {
-            const translationdata = await getDictionary(params.lang);
-            setDict(translationdata);
-        })();
-    })
+export default function Card() {
+    const { lang, slugStr } = useApp();
+    const Api = process.env.NEXT_PUBLIC_API
 
     return (
         <>
-            <MobileHeader type="Third"  lang={params.lang} pageTitle={params.lang === 'ar' ? 'بوابة الدفع الآمنة' : 'Secure Payment'} />
+            <MobileHeader type="Third"  lang={lang} pageTitle={lang === 'ar' ? 'بوابة الدفع الآمنة' : 'Secure Payment'} />
             <div className="container py-32 md:py-32">
-                <iframe className='w-full h-[800px]' src={Api + 'hyperpay/' + params.slug + '/' + params.lang}></iframe>
+                <iframe className='w-full h-[800px]' src={Api + 'hyperpay/' + slugStr + '/' + lang}></iframe>
             </div>
         </>
     )
