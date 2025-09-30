3cc31e2d092140b4477c83ad50ba14013db1ede0
Muhammad Usman Siddiqui <tamkeenstores@gmail.com>
2025-09-30 15:39:49 +0300

    update

--- app/error.tsx
+++ app/error.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import { useEffect } from "react";
-import Image from "next/image";
-import NotFoundIcon from "@/public/icons/error-404.png";
+import LottieAnimation from "@/components/LottieAnimation";
+import Link from "next/link";
 
 export default function GlobalError({
     error,
@@ -17,24 +17,14 @@ export default function GlobalError({
     }, [error]);
 
     return (
-        <html>
-            <body>
-                <div className="nc__278mainDiv">
-                    <div className="flex flex-col items-center justify-center text-center p-14">
-                        <Image
-                            src={NotFoundIcon}
-                            alt="Not Found"
-                            title="Not Found"
-                            width={0}
-                            height={0}
-                            className="w-xs h-[230px]"
-                        />
-                        <h3 className="text-[#404553] text-[22px] font-semibold">We couldn't find what you were looking for</h3>
-                        <p className="text-[#7e859b] text-sm mt-2">We are very sorry but something has gone wrong, please try again</p>
-                        <button onClick={() => reset()} className="btn nc__278mainInnerLink !mt-6">Back to Home</button>
-                    </div>
-                </div>
-            </body>
-        </html>
+        <div className="flex flex-col items-center justify-center text-center p-14">
+            <LottieAnimation src="/json/error.json" loop width={200} height={200} />
+            <h1 className="text-[#404553] text-[22px] font-semibold">We couldn't find what you were looking for</h1>
+            <p className="text-[#7e859b] text-sm mt-2">We are very sorry but something has gone wrong, please try again</p>
+            {/* 👇 Use plain <a> so it doesn’t resubmit the wrong path */}
+            <Link href="/" className="btn nc__278mainInnerLink mt-6">
+                Back to Home
+            </Link>
+        </div>
     );
 }
