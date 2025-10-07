890ef5cfd8e08dc82f1e250028be712dbd9aef7a
Adeel Khan <adeel@techenix.com>
2025-10-07 12:14:39 +0500

    update

--- app/[lang]/components/LoyaltyHistoryList.tsx
+++ app/[lang]/components/LoyaltyHistoryList.tsx
@@ -1,54 +1,70 @@
 "use client";
 
 import dayjs from "dayjs";
-import 'dayjs/locale/ar'
+import "dayjs/locale/ar";
 import React from "react";
+import SARIcon from "./Icons/SARIcon";
 
 export default function LoyaltyHistoryList(props: any) {
-    const loyalData = props?.data;
-    const isArabic = props?.isArabic;
-    const isDeducted = loyalData?.type == 0 ? true : false;
+  const loyalData = props?.data;
+  const isArabic = props?.isArabic;
+  const isDeducted = loyalData?.type == 0 ? true : false;
   return (
-    <>
       <div className="flex items-center gap-5 p-3 rounded-md bg-white shadow-md mb-3 w-full">
-        <div className="flex flex-col items-center shrink-0 bg-[#EBEBEB] py-3 px-4 rounded-md">
-          <span className="text-base font-bold text-dark">{dayjs(loyalData?.date).locale(isArabic ? "ar" : "en").format("DD")}</span>
-          <span className="text-base font-bold text-dark">{dayjs(loyalData?.date).locale(isArabic ? "ar" : "en").format("MMM")}</span>
+        <div className="flex flex-col items-center shrink-0 bg-[#f1f1f1] py-2 px-4 rounded-md">
+          <span className="text-base font-bold text-dark">
+            {dayjs(loyalData?.date)
+              .locale(isArabic ? "ar" : "en")
+              .format("DD")}
+          </span>
+          <span className="text-sm leading-3.5 text-dark">
+            {dayjs(loyalData?.date)
+              .locale(isArabic ? "ar" : "en")
+              .format("MMM")}
+          </span>
         </div>
         <div className="grow">
-          <p className="text-sm font-semibold text-dark ml-3 mb-1">
+          <p className="text-sm leading-3.5 font-bold text-dark">
+            {isDeducted
+              ? isArabic
+                ? "رقم الطلب"
+                : "Order no."
+              : ''}
+          </p>
+          <p className="text-sm font-semibold text-dark">
             {loyalData?.earning_against}
           </p>
-          <span className="text-xs text-dark flex items-center gap-1 font-semibold">
-            {isArabic ? 'المبلغ:' : 'Amount:'} {loyalData?.total_amount}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="12"
-              height="11"
-              viewBox="0 0 10 10"
-              fill="none"
-            >
-              <path
-                d="M5.8684 8.15245C5.73054 8.45815 5.63941 8.78989 5.60449 9.13779L8.52202 8.51759C8.65989 8.21197 8.75095 7.88015 8.78593 7.53226L5.8684 8.15245Z"
-                fill="currentColor"
-              />
-              <path
-                d="M8.42583 6.65928C8.56369 6.35365 8.65482 6.02183 8.68974 5.67394L6.41707 6.15729V5.22811L8.42576 4.80124C8.56362 4.49561 8.65476 4.1638 8.68967 3.8159L6.41701 4.29885V0.957207C6.06877 1.15274 5.7595 1.413 5.50809 1.72001V4.49211L4.59918 4.6853V0.502716C4.25094 0.698175 3.94167 0.958513 3.69026 1.26552V4.87842L1.65657 5.31058C1.5187 5.61621 1.4275 5.94802 1.39252 6.29592L3.69026 5.80761V6.97776L1.22778 7.50104C1.08991 7.80667 0.99878 8.13849 0.963867 8.48638L3.5414 7.93863C3.75123 7.89499 3.93157 7.77093 4.04881 7.60022L4.52152 6.89941V6.89927C4.57059 6.82676 4.59918 6.73934 4.59918 6.64519V5.61442L5.50809 5.42123V7.2796L8.42576 6.65914L8.42583 6.65928Z"
-                fill="currentColor"
-              />
-            </svg>
+          <span className="text-xs text-dark flex items-center gap-1">
+            {isArabic ? "المبلغ:" : "Amount:"} <SARIcon size={9} color="#000000" /> {Number(loyalData?.total_amount).toLocaleString()}
+            
           </span>
         </div>
         <div className="flex flex-col shrink-0 pr-4">
-          <span className={`text-base ${!isDeducted ? "text-primary" : "text-red-600"} font-semibold mb-1 ml-2`}>
-            {loyalData?.t_loyaltypoints} {isArabic ? 'نقاط' : 'pts' }
+          <span
+            className={`text-sm leading-3.5 ${
+              !isDeducted ? "text-greenDark" : "text-red-600"
+            } font-semibold mb-1`}
+          >
+            {loyalData?.t_loyaltypoints
+              ? Number(loyalData.t_loyaltypoints).toLocaleString()
+              : 0}{" "}
+            {isArabic ? "نقاط" : "pts"}
           </span>
-          <span className={`text-xs ${!isDeducted ? "text-primary" : "text-red-600"} font-semibold`}>{isDeducted
-            ? (isArabic ? "تم الخصم" : "DEDUCTED")
-            : (isArabic ? "تم الإضافة" : "CREDITED")}
+
+          <span
+            className={`text-xs ${
+              !isDeducted ? "text-greenDark" : "text-red-600"
+            }`}
+          >
+            {isDeducted
+              ? isArabic
+                ? "تم الخصم"
+                : "DEDUCTED"
+              : isArabic
+              ? "تم الإضافة"
+              : "CREDITED"}
           </span>
         </div>
       </div>
-    </>
   );
 }
