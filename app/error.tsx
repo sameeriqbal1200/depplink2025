aee5d24f359b209854a8274d622992da9892b349
Irfan Web <hi0487535@gmail.com>
2025-10-11 15:45:03 +0500

    not found , error page event error

--- app/error.tsx
+++ app/error.tsx
@@ -6,6 +6,7 @@ import LottieAnimation from "@/components/LottieAnimation";
 import Link from "next/link";
 import { useApp } from "./_ctx/AppContext";
 import { getPushMessageData } from "@/lib/not_found/not_found.client";
+import { ErrorTracker } from "./[lang]/utils/errorTracker";
 
 export default function GlobalError({
     error,
@@ -17,11 +18,18 @@ export default function GlobalError({
     useEffect(() => {
         console.error(error);
     }, [error]);
-    const { origin } = useApp();
+    const { origin, deviceType, lang } = useApp();
     useEffect(()=> {
         getData()
     })
-    const getData = async () =>{
+    const getData = async () => {
+        const trackResult = ErrorTracker.trackCustomError(
+            error.message,
+            'frontend',
+            500,
+            deviceType,
+            'Error Boundary Page',
+        );
         const PreRoute = sessionStorage.getItem('preLoginRoute');
         const fullUrl = `${origin}${PreRoute}`
         const res = await getPushMessageData(fullUrl)
@@ -30,11 +38,21 @@ export default function GlobalError({
     return (
         <div className="flex flex-col items-center justify-center text-center p-14">
             <LottieAnimation src="/json/error.json" loop width={200} height={200} />
-            <h1 className="text-[#404553] text-[22px] font-semibold">We couldn't find what you were looking for</h1>
-            <p className="text-[#7e859b] text-sm mt-2">We are very sorry but something has gone wrong, please try again</p>
+            <h1 className="text-[#404553] text-[22px] font-semibold">
+            <p>
+                {lang === "en"
+                    ? "We couldn't find what you were looking for."
+                    : "لم نتمكن من العثور على ما تبحث عنه."}
+            </p>
+            </h1>
+            <p className="text-[#7e859b] text-sm mt-2">
+            {lang === "en"
+                ? "We are very sorry but something has gone wrong, please try again."
+                : "نحن آسفون جدًا، حدث خطأ ما، يرجى المحاولة مرة أخرى."}
+            </p>
             {/* 👇 Use plain <a> so it doesn’t resubmit the wrong path */}
             <Link href="/" className="btn nc__278mainInnerLink mt-6">
-                Back to Home
+                {lang === "en" ? "Back to Home" : "العودة إلى الصفحة الرئيسية"}
             </Link>
         </div>
     );
