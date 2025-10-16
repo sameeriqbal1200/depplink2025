90faa0967c2c3d140607df14f34df0847641d836
Abdul Hadi <abdulhadirathore018@gmail.com>
2025-10-16 20:09:57 +0500

    update

--- app/[lang]/account/loyaltyhistory/page.tsx
+++ app/[lang]/account/loyaltyhistory/page.tsx
@@ -8,6 +8,7 @@ import { useApp } from "@/app/_ctx/AppContext";
 import { getLoyaltyHistoryData } from "@/lib/accounts/loyaltyData.client";
 import { useSlot } from "@/app/_ctx/ClientDataRegistry";
 import LoyaltyHistoryList from "../../components/LoyaltyHistoryList";
+import SARIcon from "../../components/Icons/SARIcon";
 
 const MobileHeader = dynamic(() => import("../../components/MobileHeader"), {
   ssr: true,
@@ -17,6 +18,7 @@ export default function UserLoyaltyHistory() {
   const { lang, deviceType, origin } = useApp();
   const footer = useSlot<any>("footer");
   const [totalAvailablePoint, setTotalAvailablePoint] = useState<any>(0);
+  const [totalAvailableAmount, setTotalAvailableAmount] = useState<any>(0);
   const [totalRedeemPoint, setTotalRedeemPoint] = useState<any>(0);
   const router = useRouter();
   const path = usePathname();
@@ -47,7 +49,9 @@ export default function UserLoyaltyHistory() {
 
       const lastLoyaltyData = userLoyaltyData?.userLoyaltyData?.mainLoyalty;
       const totalPoints = lastLoyaltyData?.t_loyaltypoints ?? 0;
+      const totalAmount = lastLoyaltyData?.total_amount ?? 0;
       setTotalAvailablePoint(totalPoints);
+      setTotalAvailableAmount(totalAmount);
       setTotalRedeemPoint(redeemPointSum);
     } catch (err) {
       console.error("Failed to fetch loyalty history:", err);
@@ -65,39 +69,18 @@ export default function UserLoyaltyHistory() {
       <MobileHeader
         type="Third"
         lang={lang}
-        pageTitle={
-          lang === "ar" ? "سجل استخدام الولاء" : "Loyalty Usage History"
-        }
+        pageTitle={isArabic ? "نشاط الولاء" : "Loyalty Activity"}
       />
       <div className="container py-16">
-        <div className="loyaltyHistory_header p-3 rounded-lg bg-white shadow-md mb-3 w-full">
-          <div className="flex items-center gap-8 pt-2 pb-12 text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              className="bi bi-gift-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A3 3 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43zM9 3h2.932l.023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zm6 4v7.5a1.5 1.5 0 0 1-1.5 1.5H9V7zM2.5 16A1.5 1.5 0 0 1 1 14.5V7h6v9z" />
-            </svg>
-            <div className="flex flex-col">
-              <p className="text-sm leading-3.5 font-bold text-white mb-0.5">
-                {isArabic ? "إجمالي النقاط المستردة" : "Total redeemed points"}
-              </p>
+        <div className="rounded-lg shadow-md mb-3 w-full bg-primary">
+          <div className="text-white loyaltyHistory_header_two p-3">
+            <p className="text-xs">{isArabic ? "الرصيد المتاح:" : "Available balance:"}</p>
+            <div className="flex items-center gap-2 mt-1">
+              <SARIcon size={20} color="white" />
+              <h1 className="text-3xl font-bold">{(Number(totalAvailableAmount)).toLocaleString()}</h1>
             </div>
-            <div className="flex flex-col">
-              <p className="text-sm leading-3.5 font-bold text-white mb-0.5">
-                {totalRedeemPoint}
-              </p>
-              {/* <span className="text-xs text-white">-4,431 pts</span> */}
-            </div>
+            <p className="text-xs flex items-center gap-1 mt-4">{isArabic ? "إجمالي النقاط المتاحة:" : "Total available points:"} <span className="text-white font-bold">{Number(totalAvailablePoint).toLocaleString()}</span></p>
           </div>
-          <p className="text-base text-dark font-bold text-center mb-4">{totalAvailablePoint}</p>
-          <span className="text-xs text-dark block text-center">
-            {isArabic ? "النقاط المتاحة" : "Total Available Points"}
-          </span>
         </div>
         {loyaltyData?.map((item: any, i: number) => {
           return (
--- app/[lang]/components/LoyaltyHistoryList.tsx
+++ app/[lang]/components/LoyaltyHistoryList.tsx
@@ -10,65 +10,32 @@ export default function LoyaltyHistoryList(props: any) {
   const isArabic = props?.isArabic;
   const isDeducted = loyalData?.type == 0 ? true : false;
   return (
-    <div className="flex items-center gap-5 p-3 bg-white rounded-md shadow-md mb-3 w-full">
-      <div className="flex flex-col items-center shrink-0 bg-[#f1f1f1] py-2 px-4 rounded-md">
-        <span className="text-base font-bold text-dark">
-          {dayjs(loyalData?.date)
-            .locale(isArabic ? "ar" : "en")
-            .format("DD")}
-        </span>
-        <span className="text-sm leading-3.5 text-dark">
-          {dayjs(loyalData?.date)
-            .locale(isArabic ? "ar" : "en")
-            .format("MMM")}
+    <div className="flex items-center justify-between p-3 bg-white rounded-md shadow-md mb-2 w-full">
+      <div>
+        <p className="text-sm font-semibold text-dark -mb-2">{loyalData?.earning_against}</p>
+        <span className="text-[0.65rem] font-medium text-dark/60">
+          {dayjs(loyalData?.date).locale(isArabic ? "ar" : "en").format("MMM DD, YYYY")} at {dayjs(loyalData?.date).locale(isArabic ? "ar" : "en").format("hh:mm A")}
         </span>
       </div>
-      <div className="grow">
-        <p className="text-sm leading-3.5 font-bold text-dark">
-          {isDeducted ? (isArabic ? "رقم الطلب" : "Order no.") : ""}
-        </p>
-        <p className="text-sm font-semibold text-dark">
-          {loyalData?.earning_against}
-        </p>
-        {/* {isDeducted && ( */}
-        <span className="text-xs text-dark flex items-center gap-1">
-          {isArabic ? "المبلغ:" : "Amount:"}{" "}
-          <SARIcon size={9} color="#000000" />{" "}
-          {Number(loyalData?.total_amount).toLocaleString()}
-        </span>
-        {/* )} */}
-      </div>
-      <div className="flex flex-col shrink-0 pr-4">
-        <span
-          className={`text-sm leading-3.5 ${
-            !isDeducted ? "text-greenDark" : "text-red-600"
-          } font-semibold mb-1`}
-        >
-          {loyalData?.loyalty_points ? (
-            <>
-              {isDeducted ? "-" : "+"}
-              {Number(loyalData.loyalty_points).toLocaleString()}{" "}
-            </>
-          ) : (
-            "0 "
-          )}
-          {isArabic ? "نقاط" : "pts"}
-        </span>
-
-        {/* <span
-            className={`text-xs ${
-              !isDeducted ? "text-greenDark" : "text-red-600"
-            }`}
-          >
-            {isDeducted
-              ? isArabic
-                ? "تم الخصم"
-                : "DEDUCTED"
-              : isArabic
-              ? "تم الإضافة"
-              : "CREDITED"}
-          </span> */}
-      </div>
+      <span className={`text-sm leading-3.5 flex text-right ${!isDeducted ? "text-greenDark" : "text-[#e8000b]"} font-semibold mb-1`}>
+        {loyalData?.loyalty_points ? (
+          <div className="flex items-center gap-2">
+            {isDeducted ? <div className="text-xl">-</div> : <div className="text-xl">+</div>}
+            <div className="text-xs w-full flex flex-col text-right">
+              <div className="flex items-center justify-end gap-1">
+                {Number(loyalData.loyalty_points).toLocaleString()}{" "}
+                {isArabic ? "نقاط" : "pts"}
+              </div>
+              <div className="flex items-center justify-end gap-1 font-semibold">
+                <SARIcon size={10} />{" "}
+                {Number(loyalData?.total_amount).toLocaleString()}
+              </div>
+            </div>
+          </div>
+        ) : (
+          "0"
+        )}
+      </span>
     </div>
   );
 }
