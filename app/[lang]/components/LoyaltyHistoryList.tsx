366eebc2df892e8640f4b0aa082dbc57c473cf6e
Irfan Web <hi0487535@gmail.com>
2025-10-24 19:06:59 +0500

    loyalty fix

--- app/[lang]/components/LoyaltyHistoryList.tsx
+++ app/[lang]/components/LoyaltyHistoryList.tsx
@@ -9,6 +9,7 @@ export default function LoyaltyHistoryList(props: any) {
   const loyalData = props?.data;
   const isArabic = props?.isArabic;
   const isDeducted = loyalData?.type == 0 ? true : false;
+  const isRefunded = loyalData?.is_refund == 1 ? true : false;
   const isPending = loyalData?.date ? dayjs().isBefore(dayjs(loyalData.date).add(8, "days")) : false;
   return (
     <div className="flex items-center justify-between p-3 bg-white rounded-md shadow-md mb-2 w-full">
@@ -20,13 +21,21 @@ export default function LoyaltyHistoryList(props: any) {
       </div>
       <span
         className={`text-sm leading-3.5 flex text-right font-semibold mb-1 ${
-          isPending ? "text-orange" : !isDeducted ? "text-greenDark" : "text-[#e8000b]"
+          !isDeducted && isPending && !isRefunded
+            ? "text-orange"
+            : !isDeducted
+            ? "text-greenDark"
+            : "text-[#e8000b]"
         }`}
       >
         {loyalData?.loyalty_points ? (
           <div className="flex items-center gap-2">
-            {isPending && (<div className="text-xs text-orange-500 font-semibold">({isArabic ? "قيد الانتظار" : "Pending"})</div>)}
-            {isDeducted ? <div className="text-xl">-</div> : <div className="text-xl">+</div>}
+            {!isDeducted && isPending && !isRefunded && (
+              <div className="text-xs text-orange-500 font-semibold">
+                {isArabic ? "قيد الانتظار" : "Pending"}
+              </div>
+            )}
+            {(isDeducted || isRefunded) ? <div className="text-xl">-</div> : <div className="text-xl">+</div>}
             <div className="text-xs w-full flex flex-col text-right">
               <div className="flex items-center justify-end gap-1">
                 {Number(loyalData.loyalty_points).toLocaleString()}{" "}
